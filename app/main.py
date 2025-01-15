ed04bc05d916a1c6e6a1712dbd40792e07253de8
Libin Faby <libinfc0@gmail.com>
2025-01-15 06:47:51 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
